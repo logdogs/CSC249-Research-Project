bef0e2e338aaa6a9070032a4d000039b87cb6120
logdogs <59925272+logdogs@users.noreply.github.com>
2021-05-01 18:40:24 -0400

    Cleaned the code a bit and replaced the Godforsaken smikage shit with the God-fearing opencv equivalents. Let's kick some Chinese character ass. In nomine patris et filii et spiritus sancti, Amen.

--- primer.py
+++ primer.py
@@ -5,10 +5,8 @@ import imageio
 import matplotlib.pyplot as plt
 import PIL.Image as im
 import subprocess
-import decomp_lookup
 import sys
 import comparisons
-import os
 
 # Function to remove the shadows from an image so that our method for finding the character will work
 def remove_shadows(image):
@@ -119,67 +117,79 @@ def run_CNN(input_name,save_name):
     save(to_save,save_name)
     subprocess.call("python model_test.py " + save_name, shell=True)
 
-from skimage.morphology import skeletonize
-import matplotlib
-import matplotlib.pyplot as plt
-import skimage.io as io
-import numpy
-"load image data"
+# # from skimage.morphology import skeletonize
+# import matplotlib
+# import matplotlib.pyplot as plt
+# # import skimage.io as io
+# import numpy
+# "load image data"
 
-"Convert gray images to binary images using Otsu's method"
-from skimage.filters import threshold_otsu
-#Otsu_Threshold = threshold_otsu(Img_Original)   
-#BW_Original = Img_Original < Otsu_Threshold    # must set object region as 1, background region as 0 !
+# "Convert gray images to binary images using Otsu's method"
+# # from skimage.filters import threshold_otsu
+# #Otsu_Threshold = threshold_otsu(Img_Original)   
+# #BW_Original = Img_Original < Otsu_Threshold    # must set object region as 1, background region as 0 !
 
-def neighbours(x,y,image):
-    "Return 8-neighbours of image point P1(x,y), in a clockwise order"
-    img = image
-    x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
-    return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],     # P2,P3,P4,P5
-                img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]    # P6,P7,P8,P9
+# def neighbours(x,y,image):
+#     "Return 8-neighbours of image point P1(x,y), in a clockwise order"
+#     img = image
+#     x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
+#     return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],     # P2,P3,P4,P5
+#                 img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]    # P6,P7,P8,P9
 
-def transitions(neighbours):
-    "No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"
-    n = neighbours + neighbours[0:1]      # P2, P3, ... , P8, P9, P2
-    return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
+# def transitions(neighbours):
+#     "No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"
+#     n = neighbours + neighbours[0:1]      # P2, P3, ... , P8, P9, P2
+#     return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
 
-def zhangSuen(image):
-    "the Zhang-Suen Thinning Algorithm"
-    Image_Thinned = image.copy()  # deepcopy to protect the original image
-    changing1 = changing2 = 1        #  the points to be removed (set as 0)
-    while changing1 or changing2:   #  iterates until no further changes occur in the image
-        # Step 1
-        changing1 = []
-        rows, columns = Image_Thinned.shape               # x for rows, y for columns
-        for x in range(1, rows - 1):                     # No. of  rows
-            for y in range(1, columns - 1):            # No. of columns
-                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
-                if (Image_Thinned[x][y] == 1     and    # Condition 0: Point P1 in the object regions 
-                    2 <= sum(n) <= 6   and    # Condition 1: 2<= N(P1) <= 6
-                    transitions(n) == 1 and    # Condition 2: S(P1)=1  
-                    P2 * P4 * P6 == 0  and    # Condition 3   
-                    P4 * P6 * P8 == 0):         # Condition 4
-                    changing1.append((x,y))
-        for x, y in changing1: 
-            Image_Thinned[x][y] = 0
-        # Step 2
-        changing2 = []
-        for x in range(1, rows - 1):
-            for y in range(1, columns - 1):
-                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
-                if (Image_Thinned[x][y] == 1   and        # Condition 0
-                    2 <= sum(n) <= 6  and       # Condition 1
-                    transitions(n) == 1 and      # Condition 2
-                    P2 * P4 * P8 == 0 and       # Condition 3
-                    P2 * P6 * P8 == 0):            # Condition 4
-                    changing2.append((x,y))    
-        for x, y in changing2: 
-            Image_Thinned[x][y] = 0
-    return Image_Thinned
+# def zhangSuen(image):
+#     "the Zhang-Suen Thinning Algorithm"
+#     Image_Thinned = image.copy()  # deepcopy to protect the original image
+#     changing1 = changing2 = 1        #  the points to be removed (set as 0)
+#     while changing1 or changing2:   #  iterates until no further changes occur in the image
+#         # Step 1
+#         changing1 = []
+#         rows, columns = Image_Thinned.shape               # x for rows, y for columns
+#         for x in range(1, rows - 1):                     # No. of  rows
+#             for y in range(1, columns - 1):            # No. of columns
+#                 P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
+#                 if (Image_Thinned[x][y] == 1     and    # Condition 0: Point P1 in the object regions 
+#                     2 <= sum(n) <= 6   and    # Condition 1: 2<= N(P1) <= 6
+#                     transitions(n) == 1 and    # Condition 2: S(P1)=1  
+#                     P2 * P4 * P6 == 0  and    # Condition 3   
+#                     P4 * P6 * P8 == 0):         # Condition 4
+#                     changing1.append((x,y))
+#         for x, y in changing1: 
+#             Image_Thinned[x][y] = 0
+#         # Step 2
+#         changing2 = []
+#         for x in range(1, rows - 1):
+#             for y in range(1, columns - 1):
+#                 P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
+#                 if (Image_Thinned[x][y] == 1   and        # Condition 0
+#                     2 <= sum(n) <= 6  and       # Condition 1
+#                     transitions(n) == 1 and      # Condition 2
+#                     P2 * P4 * P8 == 0 and       # Condition 3
+#                     P2 * P6 * P8 == 0):            # Condition 4
+#                     changing2.append((x,y))    
+#         for x, y in changing2: 
+#             Image_Thinned[x][y] = 0
+#     return Image_Thinned
  
-
-
-
+# https://medium.com/analytics-vidhya/skeletonization-in-python-using-opencv-b7fa16867331
+def skeletonize(image):
+    ret,img = cv2.threshold(image,0,255,0)
+    size = np.size(img)
+    skel = np.zeros_like(img)
+    element =  cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+    while True:
+        open = cv2.morphologyEx(img, cv2.MORPH_OPEN, element)
+        temp = cv2.subtract(img,open)
+        eroded = cv2.erode(img,element)
+        skel = cv2.bitwise_or(skel,temp)
+        img = eroded.copy()
+        if cv2.countNonZero(img) == 0:
+            break
+    return skel
 
 def main():
     args = sys.argv
@@ -191,29 +201,8 @@ def main():
     sigma = convert_for_CNN(input_name)
     sigma_prime = imageio.imread("res.png")
 
-
-    #sigma_skel = sigma      # Gray image, rgb images need pre-conversion
-    #sigma[sigma!=0] = 1
-    #sigma_skel = skeletonize(sigma)
-    sigma = cv2.GaussianBlur(sigma,(3,3),0)
-    th, sigma = cv2.threshold(sigma, 1, 255, cv2.THRESH_BINARY_INV)
-    #sigma_skel = numpy.invert(sigma.astype(np.int))
-    #sigma_skel = numpy.invert(sigma)
-    sig_Threshold = threshold_otsu(sigma)
-    sigma_skel = sigma < sig_Threshold
-    sigma_skel = zhangSuen(sigma_skel)
-
-    #sigma_prime[sigma_prime!=0] = 1
-    #sigma_prime_skel = skeletonize(sigma_prime)
-    #sigma_prime_skel =  sigma_prime      # Gray image, rgb images need pre-conversion
-    sigma_prime_skel = numpy.invert(sigma_prime.astype(np.int))
-    sig_prime_Threshold = threshold_otsu(sigma_prime_skel)
-    sigma_prime_skel = sigma_prime_skel < sig_prime_Threshold
-    sigma_prime_skel = zhangSuen(sigma_prime_skel)
-    
-    "Apply the algorithm on images"
-    #BW_Skeleton = zhangSuen(BW_Original)
-    # BW_Skeleton = BW_Original
+    sigma_skel = skeletonize(sigma)
+    sigma_prime_skel = skeletonize(sigma_prime)
 
     "Display the results"
     fig, ax = plt.subplots(1, 2)
@@ -237,13 +226,6 @@ def main():
     plt.show()
 
 
-    comparisons.structural_similarity(sigma_skel,sigma_prime_skel)
-    # comparisons.ombb(sigma)
-    # comparisons.ombb(sigma_prime)
-    # plt.figure()
-    # plt.imshow(sigma,cmap='gray')
-    # plt.show()
-    # os.remove(file_name)
-    
+    comparisons.structural_similarity(sigma_skel,sigma_prime_skel)   
 
 main()
